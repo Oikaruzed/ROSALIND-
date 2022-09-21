a3accde9a0c57601b6c7c31108f43e59c0ca86c3
Oikaruzed <92225091+Oikaruzed@users.noreply.github.com>
2022-09-21 16:54:14 -0700

    Add files via upload

---
+++ 2-DNA_to_RNA.py
@@ -0,0 +1,15 @@
+#An approach to replace a letter in a string with a different letter
+DNA_sequence='GATGGAACTTGACTACGTAAATT'
+
+#variable.replace('X','Y') will replace the 'X' with 'Y' in the variable's string
+RNA_sequence=(DNA_sequence.replace('T','U'))
+    #RNA_sequence will now contain U's instead of T's
+
+print(RNA_sequence)
+
+
+
+
+
+
+
